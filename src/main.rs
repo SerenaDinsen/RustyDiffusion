027cbc15731e6e685d4004c29b839e0d18059958
Serena Dinsen <troelsdinsen@gmail.com>
2023-09-19 11:24:39 +0200

    another commit stuff

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,13 @@ use eframe::egui;
 
 fn main() {
     let native_options = eframe::NativeOptions::default();
-    eframe::run_native("RustyDiffusion", native_options, Box::new(|cc| Box::new(rustydiffusion::new(cc))));
+    eframe::run_native("RustyDiffusion", native_options, Box::new(|cc| Box::new(RustyDiffusion::new(cc))));
 }
 
 #[derive(Default)]
-struct rustydiffusion {}
+struct RustyDiffusion {}
 
-impl rustydiffusion {
+impl RustyDiffusion {
     fn new(cc: &eframe::CreationContext<'_>) -> Self {
         // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
         // Restore app state using cc.storage (requires the "persistence" feature).
@@ -18,7 +18,7 @@ impl rustydiffusion {
     }
 }
 
-impl eframe::App for rustydiffusion {
+impl eframe::App for RustyDiffusion {
    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
        egui::CentralPanel::default().show(ctx, |ui| {
            ui.heading("Welcome to RustyDiffusion");
