bb684bfb592d50069f33becf87e3c0661ea70f56
Serena Dinsen <troelsdinsen@gmail.com>
2023-09-19 10:25:42 +0200

    Working window gui

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,27 @@
-//this is a comment!!!
+use eframe::egui;
 
 fn main() {
-    println!("Hello, world!");
+    let native_options = eframe::NativeOptions::default();
+    eframe::run_native("RustyDiffusion", native_options, Box::new(|cc| Box::new(rustydiffusion::new(cc))));
+}
+
+#[derive(Default)]
+struct rustydiffusion {}
+
+impl rustydiffusion {
+    fn new(cc: &eframe::CreationContext<'_>) -> Self {
+        // Customize egui here with cc.egui_ctx.set_fonts and cc.egui_ctx.set_visuals.
+        // Restore app state using cc.storage (requires the "persistence" feature).
+        // Use the cc.gl (a glow::Context) to create graphics shaders and buffers that you can use
+        // for e.g. egui::PaintCallback.
+        Self::default()
+    }
+}
+
+impl eframe::App for rustydiffusion {
+   fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+       egui::CentralPanel::default().show(ctx, |ui| {
+           ui.heading("Welcome to RustyDiffusion");
+       });
+   }
 }
