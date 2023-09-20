26c095b6b4ccceb77523d276f2648b59ac02af84
Serena Dinsenm <troelsdinsen@gmail.com>
2023-09-20 19:21:37 +0200

    Fixed everything because it was shit...

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,21 @@
-use eframe::egui;
+use eframe::egui::{self, TopBottomPanel};
+use eframe::epaint::Color32;
 use std::fs::File;
 use std::io::prelude::*;
 
 fn main() {
     let native_options = eframe::NativeOptions::default();
-    eframe::run_native("RustyDiffusion", native_options, Box::new(|cc| Box::new(RustyDiffusion::new(cc))));
+    let _ = eframe::run_native(
+        "RustyDiffusion",
+        native_options,
+        Box::new(|cc| Box::new(RustyDiffusion::new(cc))),
+    );
 }
 
 #[derive(Default)]
-struct RustyDiffusion {}
+struct RustyDiffusion {
+    errmsg: String,
+}
 
 impl RustyDiffusion {
     fn new(cc: &eframe::CreationContext<'_>) -> Self {
@@ -21,16 +28,28 @@ impl RustyDiffusion {
 }
 
 impl eframe::App for RustyDiffusion {
-   fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
-       egui::CentralPanel::default().show(ctx, |ui| {
+    fn update(&mut self, ctx: &egui::Context, frame: &mut eframe::Frame) {
+        TopBottomPanel::bottom("footer").show(ctx, |ui| {
+            ui.vertical_centered(|ui| {
+                ui.add_space(10.);
+                ui.colored_label(Color32::RED, self.errmsg.clone());
+            });
+        });
+
+        egui::CentralPanel::default().show(ctx, |ui| {
             ui.heading("Welcome to RustyDiffusion");
             ui.separator();
-
             if ui.button("Click me bitch").clicked() {
-                writefile();
+                match writefile() {
+                    Ok(v) => (),
+                    Err(e) => self.errmsg = e.to_string().clone(),
+                }
             }
-       });
-   }
+            if ui.button("CLICK FOR ERROR").clicked() {
+                self.errmsg = "FUCK FUCK FUCK".to_owned();
+            }
+        });
+    }
 }
 
 fn writefile() -> std::io::Result<()> {
@@ -38,4 +57,3 @@ fn writefile() -> std::io::Result<()> {
     file.write_all(b"Hello, world!")?;
     Ok(())
 }
-
